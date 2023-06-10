e69cf5112c794af2716fb5a37fb2f949e86c5a77
Wilson Abad <wilsonabad@pangga.ph>
2023-06-11 00:19:27 +0800

    clean

---
+++ lib/app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_launchpad_app/home_page.dart';
+
+class MyMaterialApp extends StatelessWidget {
+  const MyMaterialApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.blue,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
